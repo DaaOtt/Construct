bc57f5bc3e396f9a5b44fecbbe742f224bfa6e1a
DaaOtt <daaott@gmail.com>
2014-11-01 00:07:31 -0700

    Init Commit: Electric Boogaloo

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -2,3 +2,9 @@ AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
 
 include("shared.lua")
+
+
+function GM:PlayerSpawnObject(ply, model, skin)
+	print(ply, model, skin)
+	return true
+end
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -2,5 +2,32 @@ GM.Name = "Construct"
 GM.Author = "Ott"
 GM.Email = "N/A"
 GM.Website = "https://github.com/DaaOtt/Construct"
-
 DeriveGamemode("sandbox")
+
+print("Loading files...")
+local files, folders = file.Find("construct/gamemode/construct/*", "LUA")
+local function load(s)
+	print("Loaded file " .. s)
+	include(s)
+end
+for k, v in pairs(files) do
+	if string.sub(v, -4) == ".lua" then
+		if string.sub(v, 1, 3) == "sv_" and SERVER then
+			load("construct/" .. v)
+		end
+		if string.sub(v, 1, 3) == "cl_" and CLIENT then
+			load("construct/" .. v)
+		end
+		if string.sub(v, 1, 3) == "sh_" then
+			load("construct/" .. v)
+		end
+	end
+end
+
+function GM:PlayerNoClip(ply, s)
+	if s then
+		return ply:IsAdmin()
+	else 
+		return true
+	end
+end
