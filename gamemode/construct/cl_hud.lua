624db769d919332e6f006864eed452457adc4a89
DaaOtt <daaott@gmail.com>
2014-11-02 19:32:29 -0800

    Made health bar context-sensitive

--- gamemode/construct/cl_hud.lua
+++ gamemode/construct/cl_hud.lua
@@ -9,14 +9,17 @@ hook.Add("HUDShouldDraw", "HideHud", hidehud)
 
 local heart = Material("icon16/heart.png")
 local healthbar = {}
-healthbar.w = 8
 healthbar.h = 180
 healthbar.th = 180
 healthbar.ph = 180
-healthbar.x = 28
 healthbar.y = ScrH() - 64 - 24 - healthbar.h
 healthbar.ratio = 0
 healthbar.delta = 0
+local health = {}
+health.x = 0
+health.tx = 0
+health.px = 0
+health.ratio = 0
 
 local function updatehealth()
 	if not IsValid(LocalPlayer()) then return end
@@ -28,6 +31,21 @@ local function updatehealth()
 		healthbar.ratio = 0
 		healthbar.delta = healthbar.th - healthbar.ph
 	end
+
+	local px = health.tx
+	if LocalPlayer():Health() >= 100 then
+		health.tx = -65
+	else
+		health.tx = 0
+	end
+	if px ~= health.tx then
+		if px < health.tx then
+			health.ratio = 1
+		elseif px > health.tx then
+			health.ratio = 0
+		end
+		health.px = px
+	end
 end
 hook.Add("Tick", "UpdateHealth", updatehealth)
 
@@ -35,11 +53,18 @@ local function drawhud()
 	healthbar.ratio = math.min(1, healthbar.ratio + FrameTime() * 2)
 	healthbar.h = healthbar.ph + easing.easeOutBounce(healthbar.ratio, 1, 0, 1) * healthbar.delta
 
+	local healthchange = FrameTime() * 2
+	if health.px == -65 then
+		healthchange = FrameTime() * -2
+	end
+	health.ratio = math.max(0, math.min(1, health.ratio + healthchange))
+	health.x = easing.easeInBack(health.ratio, 1, 0, 1) * (health.tx + health.px)
+
 	surface.SetDrawColor(Color(0, 0, 0, 128))
-	surface.DrawRect(0, ScrH() - 64 - 256, 64, 256)
+	surface.DrawRect(health.x, ScrH() - 64 - 256, 64, 256)
 	surface.SetDrawColor(Color(255, 255, 255, 255))
-	surface.DrawRect(healthbar.x, healthbar.y, healthbar.w, healthbar.h)
+	surface.DrawRect(28 + health.x, healthbar.y, 8, healthbar.h)
 	surface.SetMaterial(heart)
-	surface.DrawTexturedRect(24, ScrH() - 64 - 256 + 24, 16, 16)
+	surface.DrawTexturedRect(24 + health.x, ScrH() - 64 - 256 + 24, 16, 16)
 end
 hook.Add("HUDPaint", "DrawHud", drawhud)
