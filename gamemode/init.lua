7d7691a71e492e869041d752502b53c8ee1c7366
DaaOtt <daaott@gmail.com>
2014-11-04 23:11:21 -0800

    non-hacky pre-spawn prop weighing

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -22,85 +22,17 @@ function GM:GetFallDamage(ply, speed)
 	return (speed - 526.5) * (100 / 396) -- the Source SDK value
 end
 
---[[---------------------------------------------------------
-   Name: DoPlayerEntitySpawn
-   Desc: Utility function for player entity spawning functions
------------------------------------------------------------]]
-function DoPlayerEntitySpawn( player, entity_name, model, iSkin, strBody )
-
-	local vStart = player:GetShootPos()
-	local vForward = player:GetAimVector()
-
-	local trace = {}
-	trace.start = vStart
-	trace.endpos = vStart + (vForward * 2048)
-	trace.filter = player
-	
-	local tr = util.TraceLine( trace )
-
-	-- PrintTable( tr )
-
-	-- Prevent spawning too close
-	--if ( !tr.Hit || tr.Fraction < 0.05 ) then 
-	--	return 
-	--end
-	
-	local ent = ents.Create( entity_name )
-	if not IsValid(ent) then return end
-	print(ent)
-
-	local ang = player:EyeAngles()
-	ang.yaw = ang.yaw + 180 -- Rotate it 180 degrees in my favour
-	ang.roll = 0
-	ang.pitch = 0
-	
-	if (entity_name == "prop_ragdoll") then
-		ang.pitch = -90
-		tr.HitPos = tr.HitPos
-	end
-	
-	ent:SetModel( model )
-	ent:SetSkin( iSkin )
-	ent:SetAngles( ang )
-	ent:SetBodyGroups( strBody )
-	ent:SetPos( tr.HitPos )
-	ent:Spawn()
-	ent:Activate()
-	if not IsValid(ent:GetPhysicsObject()) then
-		ent:Remove()
-		return
-	end
-	print(ent:GetPhysicsObject():GetMass())
-	
-
-	-- Attempt to move the object so it sits flush
-	-- We could do a TraceEntity instead of doing all 
-	-- of this - but it feels off after the old way
-
-	local vFlushPoint = tr.HitPos - ( tr.HitNormal * 512 )	-- Find a point that is definitely out of the object in the direction of the floor
-		vFlushPoint = ent:NearestPoint( vFlushPoint )			-- Find the nearest point inside the object to that point
-		vFlushPoint = ent:GetPos() - vFlushPoint				-- Get the difference
-		vFlushPoint = tr.HitPos + vFlushPoint					-- Add it to our target pos
-										
-	if (entity_name ~= "prop_ragdoll") then
-	
-		-- Set new position
-		ent:SetPos( vFlushPoint )
-		player:SendLua( "achievements.SpawnedProp()" )
-	
+function GM:PlayerSpawnProp(ply, mdl)
+	local prop = ents.Create("prop_physics")
+	prop:SetModel(mdl)
+	prop:Spawn()
+	local obj = prop:GetPhysicsObject()
+	if IsValid(prop) and IsValid(obj) then
+		print(obj:GetMass())
+		prop:Remove()
+		return true
 	else
-	
-		-- With ragdolls we need to move each physobject
-		local VecOffset = vFlushPoint - ent:GetPos()
-		for i=0, ent:GetPhysicsObjectCount()-1 do
-			local phys = ent:GetPhysicsObjectNum( i )
-			phys:SetPos( phys:GetPos() + VecOffset )
-		end
-		
-		player:SendLua( "achievements.SpawnedRagdoll()" )
-		
+		prop:Remove()
+		return false
 	end
-
-	return ent
-	
 end
