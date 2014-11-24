33ba81df8bd5d6b4069ccb855653a6247816c8c6
DaaOtt <daaott@gmail.com>
2014-11-24 13:18:36 -0800

    notify

--- gamemode/construct/sh_player.lua
+++ gamemode/construct/sh_player.lua
@@ -23,3 +23,27 @@ function meta:ChargeWallet(a)
 		return true
 	end
 end
+
+if SERVER then
+	util.AddNetworkString("notify")
+end
+function meta:Notify(text, enum, length, sound)
+	if CLIENT then
+		notification.AddLegacy(text, _G[enum], length)
+		if sound and sound ~= "" then
+			surface.PlaySound(sound)
+		end
+	elseif SERVER then
+		net.Start("notify")
+			net.WriteString(text)
+			net.WriteString(enum)
+			net.WriteUInt(length, 8)
+			net.WriteString(sound or "")
+		net.Send(self)
+	end
+end
+if CLIENT then
+	net.Receive("notify", function()
+		LocalPlayer():Notify(net.ReadString(), net.ReadString(), net.ReadUInt(8), net.ReadString())
+	end)
+end
